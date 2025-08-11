6f6273dadf87b8b56959e622a5f504ed76a0d5a1
Your Name <you@example.com>
2025-08-11 15:54:12 +0200

    first commit to study cargo

---
+++ 01_hello_world/hello/target/debug/deps/hello-4be9ebb5941fbcb0.d
@@ -0,0 +1,5 @@
+/home/luna/rust_exercise/01_hello_world/hello/target/debug/deps/hello-4be9ebb5941fbcb0.d: src/main.rs
+
+/home/luna/rust_exercise/01_hello_world/hello/target/debug/deps/hello-4be9ebb5941fbcb0: src/main.rs
+
+src/main.rs:
---
+++ 01_hello_world/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/home/luna/rust_exercise/01_hello_world/hello/target/debug/hello: /home/luna/rust_exercise/01_hello_world/hello/src/main.rs
